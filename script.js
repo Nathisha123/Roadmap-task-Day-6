38d1e186bc0056c53ca7d1ced047cd6d259a39e8
Nathisha123 <nathisathia@gmail.com>
2023-11-07 07:41:15 +0400

    updated

--- script.js
+++ script.js
@@ -5,49 +5,49 @@
 //     and sets the respective class properties to these values.
 
 // Answer:
-// class Movie
-// {
-//     constructor(title, studio, rating)
-//     {
-//         this.title=title;
-//         this.studio=studio;
-//         this.rating=rating;
-//     }
+class Movie
+{
+    constructor(title, studio, rating)
+    {
+        this.title=title;
+        this.studio=studio;
+        this.rating=rating;
+    }
    
-// }
-// var film = new Movie("Titanic", "ABC", "4star");
-// console.log(`Movie title is  :  ${film.title}`);
-// console.log(`Movie studio is :  ${film.studio}`);
-// console.log(`Movie rating is :  ${film.rating}`);
+}
+var film = new Movie("Titanic", "ABC", "4star");
+console.log(`Movie title is  :  ${film.title}`);
+console.log(`Movie studio is :  ${film.studio}`);
+console.log(`Movie rating is :  ${film.rating}`);
 
 // -------------------------------------------------------------------------------------------------------------
 
 
-   //b.The constructor for the class Movie will set the class property rating to "PG" as default when no rating is provided.
+  //  b.The constructor for the class Movie will set the class property rating to "PG" as default when no rating is provided.
    
-   //Answer:
-//    class Movie1
-// {
-//     constructor(title, studio, rating="PG")
-//     {
-//         this.title=title;
-//         this.studio=studio;
-//         this.rating=rating;
-//     }
-// }
-//     var film = new Movie1("Titanic", "ABC", "PG13");
-//     var film1= new Movie1("Friends", "XYZ");
-//     console.log(`Movie title is   :  ${film.title}`);
-//     console.log(`Movie rating is  :  ${film.rating}`);
-//     console.log(`Movie rating is  :  ${film1.rating}`);
+  //  Answer:
+   class Movie1
+{
+    constructor(title, studio, rating="PG")
+    {
+        this.title=title;
+        this.studio=studio;
+        this.rating=rating;
+    }
+}
+    var film = new Movie1("Titanic", "ABC", "PG13");
+    var film1= new Movie1("Friends", "XYZ");
+    console.log(`Movie title is   :  ${film.title}`);
+    console.log(`Movie rating is  :  ${film.rating}`);
+    console.log(`Movie rating is  :  ${film1.rating}`);
 
-//--------------------------------------------------------------------------------------------------------------
+// --------------------------------------------------------------------------------------------------------------
 
-//c. Write a method getPG, which takes an array of base type Movie as its argument, and returns a new array of only
+// c. Write a method getPG, which takes an array of base type Movie as its argument, and returns a new array of only
 //  those movies in the input array with a rating of "PG". You may assume the input array is full of Movie instances. 
 //  The returned array need not be full.
 
-//Answer:
+// Answer:
 // class Movie2
 // {
 //     constructor(title, studio, rating)
@@ -81,129 +81,129 @@
 
   
   
-//------------------------------------------------------------------------------------------------------------------
+// ------------------------------------------------------------------------------------------------------------------
 
-//d.  Write a piece of code that creates an instance of the class Movie with the title “Casino Royale”, the studio “Eon Productions”, and the rating “PG­13”
-//Answer:
+// d.  Write a piece of code that creates an instance of the class Movie with the title “Casino Royale”, the studio “Eon Productions”, and the rating “PG­13”
+// Answer:
 
-// class Movie3
-// {
-//     constructor(title, studio, rating)
-//     {
-//         this.title=title;
-//         this.studio=studio;
-//         this.rating=rating;
-//     }
+class Movie3
+{
+    constructor(title, studio, rating)
+    {
+        this.title=title;
+        this.studio=studio;
+        this.rating=rating;
+    }
    
-// }
-// var film = new Movie3("Casino Royale", "Eon productions", "PG13");
-// console.log(`Movie title is  :  ${film.title}`);
-// console.log(`Movie studio is :  ${film.studio}`);
-// console.log(`Movie rating is :  ${film.rating}`);
+}
+var film = new Movie3("Casino Royale", "Eon productions", "PG13");
+console.log(`Movie title is  :  ${film.title}`);
+console.log(`Movie studio is :  ${film.studio}`);
+console.log(`Movie rating is :  ${film.rating}`);
 
-//--------------------------------------------------------------------------------------------------------------------
+// --------------------------------------------------------------------------------------------------------------------
 
-//2. Circle - class
+// 2. Circle - class
 
 // Answer:
-// class Circle
-// {
-//     constructor(radius, color)
-//     {
-//         this.RADIUS=radius;
-//         this.COLOR=color;
-//     }
-//     set Radius(r)
-//     {
-//      this.RADIUS =r;
-//     }
-//     get Radius()
-//     {
-//        return this.RADIUS;
-//     }
-//     set color(c)
-//     {
-//        this.COLOR =c; 
-//     }
-//    get color()
-//    {
-//     return this.COLOR;
-//    }
-//    get tostring()
-//    {
-//     return `"Circle[radius=${this.radius},color=${this.color}]"`
-//    }
-//    get area()
-//    {
-//     return Math.PI*this.RADIUS*this.RADIUS;
-//    }
-//    get Circumference()
-//    {
-//     return 2*Math.PI*this.RADIUS;
-//    }
-// }
+class Circle
+{
+    constructor(radius, color)
+    {
+        this.RADIUS=radius;
+        this.COLOR=color;
+    }
+    set Radius(r)
+    {
+     this.RADIUS =r;
+    }
+    get Radius()
+    {
+       return this.RADIUS;
+    }
+    set color(c)
+    {
+       this.COLOR =c; 
+    }
+   get color()
+   {
+    return this.COLOR;
+   }
+   get tostring()
+   {
+    return `"Circle[radius=${this.radius},color=${this.color}]"`
+   }
+   get area()
+   {
+    return Math.PI*this.RADIUS*this.RADIUS;
+   }
+   get Circumference()
+   {
+    return 2*Math.PI*this.RADIUS;
+   }
+}
 
-// var result= new Circle (1.0, "red");   //get
-// console.log(result.Radius);          
-// result.Radius = 4                     //set
-// console.log(result.Radius);
-// console.log(result.color);
-// result.color = "Blue";
-// console.log(result.color);
-// console.log(result.tostring);
-// console.log(result.area);
-// console.log(result.Circumference);
+var result= new Circle (1.0, "red");   //get
+console.log(result.Radius);          
+result.Radius = 4                     //set
+console.log(result.Radius);
+console.log(result.color);
+result.color = "Blue";
+console.log(result.color);
+console.log(result.tostring);
+console.log(result.area);
+console.log(result.Circumference);
 
-//-------------------------------------------------------------------------------------------------
+// -------------------------------------------------------------------------------------------------
 
 
 // 3. Person class to hold all the details:
-//Answer:
+// Answer:
 
-// class Person
-// {
-//     constructor(name, age, gender, city)
-//     {
-//           this.name= name;
-//           this.age=age;
-//           this.gender=gender;
-//           this.city=city;
+class Person
+{
+    constructor(name, age, gender, city)
+    {
+          this.name= name;
+          this.age=age;
+          this.gender=gender;
+          this.city=city;
 
-//     }
-// }
-// var details = new Person("Nathisha", "27", "Female", "chennai");
-// console.log(`The name of the person is   : ${details.name}`);
-// console.log(`The age of the person is    : ${details.age}`);
-// console.log(`The gender of the person is : ${details.gender}`);
-// console.log(`The city of the person is   : ${details.city}`);
+    }
+}
+var details = new Person("Nathisha", "27", "Female", "chennai");
+console.log(`The name of the person is   : ${details.name}`);
+console.log(`The age of the person is    : ${details.age}`);
+console.log(`The gender of the person is : ${details.gender}`);
+console.log(`The city of the person is   : ${details.city}`);
 
 // ----------------------------------------------------------------------------------------------
 
 
 // 4. Calculate the uber price:
 
-//Answer:
+// Answer:
 
-// class Uber
-// {
-//     constructor (kilomtr,price=50)
-//     {
-//     this.KM= kilomtr;
-//     this.PRICE=price;
-//     }
+class Uber
+{
+    constructor (kilomtr,price=50)
+    {
+    this.KM= kilomtr;
+    this.PRICE=price;
+    }
    
-//     get price()
-//     {
-//        console.log("Getting price");
-//        return  this.KM * this.PRICE;
-//   }
-// }
-// var UBER1=new Uber(100);
-// var UBER2 = new Uber(5, 100);
-// console.log(UBER1.price);
-// console.log(UBER2.price);
+    get price()
+    {
+       console.log("Getting price");
+       return  this.KM * this.PRICE;
+  }
+}
+var UBER1=new Uber(100);
+var UBER2 = new Uber(5, 100);
+console.log(UBER1.price);
+console.log(UBER2.price);
 
-//------------------------------------------------END---------------------------------------------------------------
+// ------------------------------------------------END---------------------------------------------------------------
 
 
 
